56391bc17a77114b63a8408edb0497d87ad8faa1
Guilherme Nascimento <brcontainer@yahoo.com.br>
2016-09-13 23:08:50 -0300

    Added draw test with canvas

--- js/popoup.js
+++ js/popoup.js
@@ -7,11 +7,15 @@
     getBackground.badgeReset();
 
     var
-        statusRegex = /(^|\s)show(\s|$)/,
-        closeStatus = doc.getElementById("close-status"),
-        showStatus  = doc.getElementById("show-status"),
-        statusView  = doc.querySelector(".main .status")
+        statusRegex     = /(^|\s)show(\s|$)/,
+        closeStatus     = doc.getElementById("close-status"),
+        showStatus      = doc.getElementById("show-status"),
+        statusView      = doc.querySelector(".main .status"),
+        player          = doc.getElementById("player"),
+        playerContainer = doc.querySelector(".main .player")
     ;
+
+    var context = player.getContext("2d");
 
     function showStatusTamagotchi(e)
     {
@@ -28,5 +32,41 @@
 
     closeStatus.onclick = showStatusTamagotchi;
     showStatus.onclick  = showStatusTamagotchi;
+
+    player.height = playerContainer.clientHeight;
+    player.width = playerContainer.clientWidth;
+
+    if ("imageSmoothingEnabled" in context) {
+        context.imageSmoothingEnabled = false;
+    } else {
+        context.webkitImageSmoothingEnabled = false;
+        context.mozImageSmoothingEnabled = false;
+    }
+
+    var drawX = 0, drawY = 0;
+
+    function testDraw()
+    {
+        if (drawX > 63) {
+            drawX = 0;
+            ++drawY;
+        }
+
+        if (drawY > 57) {
+            return;
+        }
+
+        context.fillStyle = "#adadad";
+        context.fillRect(drawX * 5, drawY * 5, 5, 5);
+
+        context.fillStyle = "#000";
+        context.fillRect(drawX * 5, drawY * 5, 4, 4);
+
+        ++drawX;
+
+        setTimeout(testDraw, 1);
+    }
+
+    setTimeout(testDraw, 500);
 
 })(chrome||browser, document);
